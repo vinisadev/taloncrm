fd9c243bc7a33887a747feff8643243685dedd19
Vincenzo Fehring <vinfehring@gmail.com>
2024-12-09 16:44:13 -0600

    Remove default Nuxt page

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Welcome to Talon</h1>
+    <p>A new kind of CRM.</p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style scoped>
+h1 {
+  font-size: 2em;
+  color: #42b983;
+}
+</style>
